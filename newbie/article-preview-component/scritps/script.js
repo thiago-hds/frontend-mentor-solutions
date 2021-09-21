864d7664674cfdf52ee2d087007cb49b009e406c
Thiago Santos <s.thiagoh@gmail.com>
2021-09-21 17:39:36 -0300

    article preview component: finish challenge

--- newbie/article-preview-component/scritps/script.js
+++ newbie/article-preview-component/scritps/script.js
@@ -1,13 +1,11 @@
 const btnShare = document.querySelector('.btn-share');
 const shareOverlay = document.querySelector('.share');
-console.log(shareOverlay);
 
 const toggleShare = function () {
 	btnShare.classList.toggle('btn-share--active');
 	btnShare.classList.toggle('btn-share--inactive');
 
-	shareOverlay.classList.toggle('share--active');
-	shareOverlay.classList.toggle('share--inactive');
+	shareOverlay.classList.toggle('hidden');
 };
 
 btnShare.addEventListener('click', toggleShare);

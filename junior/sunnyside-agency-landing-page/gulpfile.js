c54c72385d94f1e16e4e8f85f32b1f063320a921
Thiago Santos <s.thiagoh@gmail.com>
2022-04-23 12:21:09 -0300

    sunnysite-agency-landing-page: structure sass with 7x1 pattern --wip

--- junior/sunnyside-agency-landing-page/gulpfile.js
+++ junior/sunnyside-agency-landing-page/gulpfile.js
@@ -1,25 +1,28 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync");
 
 function buildStyles() {
-	return gulp
-		.src('./sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./dist/css'))
-		.pipe(browserSync.stream());
+    return gulp
+        .src("./sass/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("./dist/css"))
+        .pipe(browserSync.stream());
 }
 
 function watch() {
-	browserSync.init({
-		server: {
-			useDir: './',
-		},
-	});
-	gulp.watch('./sass/**/*.scss', buildStyles);
-	gulp.watch(['./*.html', './js/**/*.js']).on('change', browserSync.reload);
+    browserSync.init({
+        server: {
+            useDir: "./",
+        },
+    });
+    gulp.watch("./sass/**/*.scss", buildStyles);
+    gulp.watch(["./*.html", "./js/**/*.js"]).on("change", browserSync.reload);
 }
 
 exports.watch = gulp.series(buildStyles, watch);
